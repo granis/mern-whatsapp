99f8259b3f8838f8c708be807c63d599813052b0
granis <granis@gmail.com>
2021-07-06 11:46:07 +0200

    add pusher listener, sync messages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios.js";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    var pusher = new Pusher("e8cd8af5dee31b7a04ab", {
+      cluster: "eu",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
